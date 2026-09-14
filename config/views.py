5893a1799f87c4aadabf282cb20474d838499f79
MACBOOK <macbook@Macshops-FDYQR7JCJQ.local>
2026-09-14 19:02:04 +0500

    django loyha 1

---
+++ config/views.py
@@ -0,0 +1,5 @@
+from django.http import HttpResponse
+# def landing_page(request):
+# 	return HttpResponse("Django loyhasi")
+def landing_page(request):
+    return HttpResponse(f"Django loyhasi: {request.META['HTTP_USER_AGENT']}")
