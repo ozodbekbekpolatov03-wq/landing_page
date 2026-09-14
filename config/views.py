d1c65d3a14e9c26dfdf87721554e5cfa381f90ff
MACBOOK <macbook@Macshops-FDYQR7JCJQ.local>
2026-09-14 19:43:59 +0500

    def funksiya uzgardi

--- config/views.py
+++ config/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-# def landing_page(request):
-# 	return HttpResponse("Django loyhasi")
-def landing_page(request):
-    return HttpResponse(f"Django loyhasi: {request.META['HTTP_USER_AGENT']}")
+ def landing_page(request):
+ 	return HttpResponse("Django loyhasi")
+#def landing_page(request):
+ #   return HttpResponse(f"Django loyhasi: {request.META['HTTP_USER_AGENT']}")
